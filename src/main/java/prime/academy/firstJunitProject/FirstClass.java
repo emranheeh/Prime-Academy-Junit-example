60df6656d285e7e3e2df410024ab23e9ea7723fd
Emran Alheeh <emranalheeh@gmail.com>
2023-08-07 22:19:01 +0300

    First commit

---
+++ src/main/java/prime/academy/firstJunitProject/FirstClass.java
@@ -0,0 +1,8 @@
+package prime.academy.firstJunitProject;
+
+public class FirstClass {
+	public int calculateEquation(int a, int b) {
+		int equation= a*b+a-b;
+		return equation;
+	}
+}
---
+++ src/test/java/prime/academy/firstJunitProjectTest/FirstClassTest.java
@@ -0,0 +1,32 @@
+package prime.academy.firstJunitProjectTest;
+
+ 
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import prime.academy.firstJunitProject.FirstClass;
+
+public class FirstClassTest {
+	private FirstClass fc1;
+	@BeforeEach
+    public void setUp() {
+        System.out.println("Setting up Calculator instance for each test");
+        fc1 = new FirstClass();
+         
+    }
+	@AfterEach
+    public void tearDown() {
+        System.out.println("Clear Calculator instance for each test");
+        fc1 = new FirstClass();
+         
+    }
+	@Test
+	public void calaculationEquationTest() {
+		System.out.println("test method ");
+		int result= fc1.calculateEquation(10,5);
+		Assertions.assertEquals(55, result );
+	}
+}
