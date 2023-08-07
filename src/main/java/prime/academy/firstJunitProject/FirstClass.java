d8d181b225aaf425b97fd88e1b5f5d380bcefe18
Emran Alheeh <emranalheeh@gmail.com>
2023-08-07 22:33:16 +0300

    Change2 code commit

--- src/main/java/prime/academy/firstJunitProject/FirstClass.java
+++ src/main/java/prime/academy/firstJunitProject/FirstClass.java
@@ -2,7 +2,7 @@ package prime.academy.firstJunitProject;
 
 public class FirstClass {
 	public int calculateEquation(int a, int b) {
-		int equation= a*b+a-b;
+		int equation= a*b+a+b;
 		return equation;
 	}
 }
